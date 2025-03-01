77f81e05b2f416ac7ad9b8ef9674afbded1ab271
Toxicattack4up <kuxarchik007@mail.ru>
2025-03-01 16:35:48 +0300

    Добавлена реализация функции добавления и сохранения сотрудника и так же добавлены комментарии

--- JsonMeneger.cpp
+++ JsonMeneger.cpp
@@ -10,7 +10,7 @@ QJsonObject JsonMeneger::LoadJSON(const QString &filePath)
 
     if(!file.open(QIODevice::ReadOnly | QIODevice::Text))
     {
-        //qDebug() << "Ошибка при открытии файла: " << file.errorString();
+        qDebug() << "Ошибка при открытии файла: " << file.errorString();
         //qDebug() << "Путь к файлу: " << fullPath;
         return QJsonObject();
     }
@@ -28,6 +28,24 @@ QJsonObject JsonMeneger::LoadJSON(const QString &filePath)
     return doc.object();
 }
 
+// Сохранение в файл JSON
+void JsonMeneger::SaveToJSON(const QString &filePath, const QJsonObject &json)
+{
+    QString fullPath = QCoreApplication::applicationFilePath() + "/" + filePath;
+
+    QFile file(fullPath);
+
+    if(!file.open(QIODevice::WriteOnly | QIODevice::Text))
+    {
+        qDebug() << "Ошибка при записи файла: " << file.errorString();
+        return;
+    }
+
+    QJsonDocument doc(json); // Преобразование в документ
+    file.write(doc.toJson(QJsonDocument::Indented)); // Записываем с отступами
+    file.close();
+}
+
 // Хеширование пароля
 QString JsonMeneger::HashPassword(const QString &password)
 {
@@ -36,13 +54,64 @@ QString JsonMeneger::HashPassword(const QString &password)
 }
 
 // _____Сотрудники_____
-void JsonMeneger::AddEmployee(const QString &_name,
-                 const QString &_login,
-                 const QString &_password,
-                 const QString &_fullName,
-                 const QString &_role,
-                 const QString &_email,
-                 const QDateTime &_registrationDate)
+void JsonMeneger::AddEmployee(const QString &_role,
+                              const QString &_login,
+                              const QString &_password,
+                              const QString &_fullName,
+                              const QString &_email)
 {
+    // После добавления окон необходимо сделать обработку вывода операций (ошибка или правильность выполенения)
 
+    // Присваиваем стандартную роль
+    UserRole role = UserRole::Unknown;
+    const QMap<UserRole, QString>& roleNames = UserRoleHelper::getRoleNames();
+
+    // Проверка роли и преобразование ее
+    for(auto i = roleNames.begin(); i != roleNames.end(); ++i)
+    {
+        if(i.value() == _role)
+        {
+            role = i.key();
+            break;
+        }
+    }
+    //Нужно добавить проверку для почты, чтобы она была отображалась нормально
+    // Хеширование
+    QString passwordHash = HashPassword(_password);
+
+    // Создание сотрудника
+    User newUser(role, _login, passwordHash, _fullName, _email);
+
+    // Дата регистрации
+    newUser.setRegistrationDate(QDateTime::currentDateTime());
+
+    // Формирование структуры для дальнейшей загрузки данных в файл
+    QJsonObject employeeObj;
+    employeeObj["role"] = roleNames.value(newUser.getRole());
+    employeeObj["login"] = newUser.getLogin();
+    employeeObj["passwordHash"] = newUser.getPasswordHash();
+    employeeObj["fullName"] = newUser.getFullName();
+    employeeObj["email"] = newUser.getEmail();
+    employeeObj["registrationDate"] = newUser.getRegistrationDate().toString(Qt::ISODate);
+
+    // Загрука файла с определенными категориями
+    QJsonObject json = LoadJSON("Base.json");
+    QJsonArray employees = json["Employees"].toArray();
+
+    // Проверка на дубль логина
+    for(const QJsonValue &value : employees)
+    {
+        if(value.toObject()["login"] == _login)
+        {
+            qDebug() << "Логин уже существует";
+            return;
+        }
+    }
+
+    // Добавление нового сотрудника в массив
+    employees.append(employeeObj);
+
+    // Загружаем массив обратно в файл и сохраняем
+    json["employees"] = employees;
+    SaveToJSON("Base.json", json);
 }
