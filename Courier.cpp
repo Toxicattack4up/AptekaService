9ba84f39d10d3aaef0d6cf2606b64b418b9c8a7b
Disappeart <klasswok@gmail.com>
2025-03-30 15:49:02 +0300

    Исправлена ошибка класса Courier

--- Courier.cpp
+++ Courier.cpp
@@ -1,3 +1,4 @@
 #include "Courier.h"
 
-Courier::Courier() {}
+Courier::Courier(const QString& login, const QString& passwordHash, const QString& fullName, const QString& email)
+    : User(UserRole::Courier, login, passwordHash, fullName, email) {}
