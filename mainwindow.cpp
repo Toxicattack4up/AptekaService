2232d4756b1d3d543cc377ab77a2bb2b741addf5
Toxicattack4up <kuxarchik007@mail.ru>
2025-03-31 20:23:13 +0300

    Добавлены окна окна авторизации, необходима реализация метода проверки логина пароля и чтобы это все работало вместе

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,20 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->login_button, &QPushButton::clicked, this, &MainWindow::on_login_button_clicked);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_login_button_clicked()
+{
+    QString login = ui->lineEdit_login->text();
+    QString pass = ui->lineEdit_pass->text();
+
+
+}
+
